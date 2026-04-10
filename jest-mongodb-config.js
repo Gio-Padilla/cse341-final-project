9026a899d43d53ef3319c277927cd5dd33a5f02c
Gio-Padilla <pad24005@byui.edu>
2026-04-10 14:59:20 -0500

    Added some testing. Run 'npm test' in order to get it to work and test the all and get all in the 4 controllers.

---
+++ jest-mongodb-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '6.0.5'
+    },
+    autoStart: false
+  }
+};
---
+++ tests/packages.test.js
@@ -0,0 +1,83 @@
+const mongodb = require('../data/database');
+const { getAll, getSingle } = require('../controllers/packages');
+
+describe('Packages Controller', () => {
+
+    // Start in-memory DB
+    beforeAll((done) => {
+        mongodb.initDb(done);
+    });
+
+    // Reset DB before each test
+    beforeEach(async () => {
+        const db = mongodb.getDatabase().db();
+
+        await db.collection('packages').deleteMany({});
+
+        await db.collection('packages').insertMany([
+            {
+                packageId: 'PKG123',
+                title: 'Test Title',
+                destinationId: 'DST123',
+                durationDays: 7,
+                price: 500,
+                includesFlight: true,
+                includesHotel: true,
+                maxPeople: 5,
+                description: 'This is a test description'
+            }
+        ]);
+    });
+
+    test('getAll returns all packages', async () => {
+        const req = {};
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await getAll(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalled();
+    });
+
+    test('getSingle returns one package', async () => {
+        const req = {
+            params: { packageId: 'PKG123' }
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await getSingle(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    test('getSingle returns 404 if not found', async () => {
+        const req = {
+            params: { packageId: '999' }
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await getSingle(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(404);
+    });
+
+    afterAll(async () => {
+        const db = mongodb.getDatabase();
+        if (db) {
+            await db.close(); // closes MongoDB connection
+        }
+    });
+
+});
